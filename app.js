1c9c33c0fd82272b093b0bf701bea1624ff445a6
devdotatb <108785089+devdotatb@users.noreply.github.com>
2023-08-17 11:44:23 +0700

    may be last version

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ const express = require('express');
 const axios = require('axios');
 const jwt = require('jsonwebtoken');
 const sql = require('mssql');
-const bodyParser = require('body-parser');
+const moment = require('moment');
+const cors = require('cors');
 
 const app = express();
+// Enable CORS for all routes
+app.use(cors());
 const port = 3333;
 const jwtSecretKey = 'JWTAuthenticationHIGHsecuredPasswordVVVp10H7Xzyr'; // Replace with a strong secret key for JWT
 
 
-// Middleware to parse JSON data from requests
-app.use(bodyParser.json());
 
 // Database configuration (replace with your actual SQL Server connection details)
 const dbConfig = {
@@ -39,21 +40,7 @@ async function fetchStockData(stockSymbol) {
   }
 }
 
-// Function to insert stock price into the database
-async function insertStockPrice(stockSymbol, closingPrice) {
-  try {
-    const pool = await sql.connect(dbConfig); // Connecting to SQL Server
-    const query = 'INSERT INTO StockPriceHistory (stock_symbol, price_date, closing_price) VALUES (@stockSymbol, GETDATE(), @closingPrice)';
-    const result = await pool.request()
-      .input('stockSymbol', sql.NVarChar, stockSymbol)
-      .input('closingPrice', sql.Decimal, closingPrice)
-      .query(query);
 
-    await sql.close(); // Closing the connection pool after the query
-  } catch (error) {
-    console.error('Error inserting stock price:', error);
-  }
-}
 
 // Middleware to verify JWT token
 function verifyToken(req, res, next) {
@@ -71,15 +58,9 @@ function verifyToken(req, res, next) {
     next();
   });
 }
-// API endpoint to get a JWT token (for demonstration purposes only)
-app.get('/getToken', (req, res) => {
-  const user = { id: 1, username: 'example_user' }; // Replace with your user data
-  const token = jwt.sign(user, jwtSecretKey, { expiresIn: '1h' }); // Token will expire in 1 hour
-  res.json({ token });
-});
 
 // Protected API endpoint to trigger the process
-app.get('/updateStockPrices', verifyToken, async (req, res) => {
+app.get('/updateStockPricesHistory', verifyToken, async (req, res) => {
   try {
     // Create a connection pool
     const pool = await sql.connect(dbConfig);
@@ -87,12 +68,27 @@ app.get('/updateStockPrices', verifyToken, async (req, res) => {
     const query = 'SELECT stock_symbol FROM StockData';
     const result = await pool.request().query(query);
 
+    const todayDate = moment().format('YYYY-MM-DD');
+
     for (const row of result.recordset) {
       const stockSymbol = row.stock_symbol;
-      const stockData = await fetchStockData(stockSymbol);
-      if (stockData) {
-        const { c: closingPrice } = stockData;
-        await insertStockPrice(stockSymbol, closingPrice);
+
+      // Check if the stock price for today already exists
+      const existingData = await checkIfStockPriceExists(stockSymbol, todayDate);
+      if (existingData) {
+        // If data exists, update the stock price
+        const stockData = await fetchStockData(stockSymbol);
+        if (stockData) {
+          const { c: closingPrice } = stockData;
+          await updateStockPrice(stockSymbol, closingPrice, todayDate);
+        }
+      } else {
+        // If data does not exist, insert the stock price
+        const stockData = await fetchStockData(stockSymbol);
+        if (stockData) {
+          const { c: closingPrice } = stockData;
+          await insertStockPrice(stockSymbol, closingPrice, todayDate);
+        }
       }
     }
 
@@ -104,6 +100,80 @@ app.get('/updateStockPrices', verifyToken, async (req, res) => {
     res.status(500).json({ error: 'An error occurred while updating stock prices.' + error.message });
   }
 });
+
+app.get('/api/singleStockPriceHistory', verifyToken, async (req, res) => {
+  const stockSymbol = req.query.stock_symbol; // Access the stock_symbol from query parameters
+  try {
+    // Connect to the SQL Server
+    const pool = await sql.connect(dbConfig);
+
+    // Query the database for the specific stock_symbol's StockPriceHistory
+    const result = await pool.request()
+      .input('stock_symbol', sql.NVarChar, stockSymbol)
+      .query('SELECT [id], [stock_symbol], [price_date], [closing_price] FROM [StockPriceHistory] WHERE [stock_symbol] = @stock_symbol ORDER BY [price_date]');
+
+    // Send the result as the response
+    res.json(result.recordset);
+  } catch (error) {
+    console.error('Error occurred while fetching data:', error);
+    res.status(500).json({ error: 'An error occurred while fetching data.' });
+  }
+});
+
+app.get('/api/stocks', verifyToken, async (req, res) => {
+  const userId = req.user.userid;
+  console.log(req.user);
+
+  try {
+    // Connect to the database
+    await sql.connect(dbConfig);
+
+    // Fetch user's stock symbols and associated stock price history
+    const userStockPriceHistoryResult = await sql.query`
+      SELECT sph.[id], us.[stock_symbol], sph.[price_date], sph.[closing_price]
+      FROM [dbo].[UsersStock] AS us
+      INNER JOIN [dbo].[StockPriceHistory] AS sph
+      ON us.[stock_symbol] = sph.[stock_symbol]
+      WHERE us.[userid] = ${userId}
+    `;
+
+    const userStockPriceHistory = userStockPriceHistoryResult.recordset;
+
+    res.json(userStockPriceHistory);
+  } catch (err) {
+    console.error('Error occurred:', err);
+    res.status(500).json({ error: 'An internal server error occurred.' });
+  } finally {
+    // Close the database connection
+    await sql.close();
+  }
+});
+
+// Helper function to check if the stock price for today already exists
+async function checkIfStockPriceExists(stockSymbol, date) {
+  const pool = await sql.connect(dbConfig);
+  const query = `SELECT * FROM StockPriceHistory WHERE stock_symbol = '${stockSymbol}' AND price_date = '${date}'`;
+  const result = await pool.request().query(query);
+  await pool.close();
+  return result.recordset.length > 0;
+}
+
+// Helper function to update the stock price for today
+async function updateStockPrice(stockSymbol, closingPrice, date) {
+  const pool = await sql.connect(dbConfig);
+  const query = `UPDATE StockPriceHistory SET closing_price = ${closingPrice} WHERE stock_symbol = '${stockSymbol}' AND price_date = '${date}'`;
+  await pool.request().query(query);
+  await pool.close();
+}
+
+// Helper function to insert the stock price for today
+async function insertStockPrice(stockSymbol, closingPrice, date) {
+  const pool = await sql.connect(dbConfig);
+  const query = `INSERT INTO StockPriceHistory (stock_symbol, closing_price, price_date) VALUES ('${stockSymbol}', ${closingPrice}, '${date}')`;
+  await pool.request().query(query);
+  await pool.close();
+}
+
 
 // Start the server
 app.listen(port, () => {
